c8a1edafe34b4fb3ee865e67b8a9c51dff376a68
1aman1 <amangupta@outlook.com>
2022-07-05 14:06:36 +0530

    day 2

---
+++ Item 5/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+int main()
+{
+    int numValues = 10;
+
+    int data[numValues];
+    std::vector<int> myvec;
+
+    std::iota(data, data + numValues, 1);
+
+    myvec.insert(myvec.begin(), data, data + numValues);
+
+    for (const auto &i : myvec)
+        std::cout << i << " ";
+
+    std::cout << std::endl;
+
+    return 0;
+}
